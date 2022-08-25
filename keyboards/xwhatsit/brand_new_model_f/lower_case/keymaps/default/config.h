c624b1c3918977a65cf265ea54e496d9f2a5d418
ImYourHuckleberry <w.tyler.krings@gmail.com>
2022-08-25 10:38:25 -0500

    whatever

--- keyboards/xwhatsit/brand_new_model_f/lower_case/keymaps/default/config.h
+++ keyboards/xwhatsit/brand_new_model_f/lower_case/keymaps/default/config.h
@@ -1,1 +1,10 @@
 #pragma once
+eeeee
+EEEEEEEEERRRR
+eeeeeeTYUUUUUUUUUUUUUUUU
+OOOOxxxx
+,,,
+,,
+xxxxxcccc
+vvvvvvvvvrrrrrrrrrrrrweweccccccccpi
+pppppppcmmppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
